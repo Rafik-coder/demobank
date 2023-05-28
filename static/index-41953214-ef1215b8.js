6185a4a53e1ac59c6fb7f39d70d7d76d4fceb73c
Code Wizard <70761045+Rafik-coder@users.noreply.github.com>
2023-05-28 00:22:52 -0400

    Add files via upload
    initial comit

---
+++ static/index-41953214-ef1215b8.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-1873ab3b-bc61c125.js";import"./sanity-fb3adb88.js";import"./index-9b1b6bd6.js";export{m as DocumentPaneProvider,a as default};
